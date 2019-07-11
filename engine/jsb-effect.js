9e3f690e232c0dddddb401034ea111f6f7a0a90b
2youyouo2 <501251991@qq.com>
2019-07-11 16:57:31 +0800

    refine effect

--- engine/jsb-effect.js
+++ engine/jsb-effect.js
@@ -4,49 +4,10 @@ const gfx = window.gfx;
 let Effect = cc.Effect;
 
 class NativeEffect extends Effect {
-    constructor(name, techniques, properties = {}, defines = {}, dependencies = [], asset = null) {
-        super(name, techniques, properties, defines, dependencies);
-
-        if (asset) {
-            var definesArr = [];
-            for (var key in defines) {
-                definesArr.push({name:key, value:defines[key]});
-            }
-        
-            this._nativeObj = new renderer.EffectNative();
-            this._nativeObj.init(JSON.stringify(asset), properties, definesArr);
-            this._nativePtr = this._nativeObj.self();
-        }
-    }
-
-    setCullMode (cullMode = gfx.CULL_BACK) {
-        this._nativeObj.setCullMode(cullMode);
-    }
-
-    setBlend (enabled = false,
-        blendEq = gfx.BLEND_FUNC_ADD,
-        blendSrc = gfx.BLEND_SRC_ALPHA,
-        blendDst = gfx.BLEND_ONE_MINUS_SRC_ALPHA,
-        blendAlphaEq = gfx.BLEND_FUNC_ADD,
-        blendSrcAlpha = gfx.BLEND_SRC_ALPHA,
-        blendDstAlpha = gfx.BLEND_ONE_MINUS_SRC_ALPHA,
-        blendColor = 0xffffffff) {
-        this._nativeObj.setBlend(blendEq, blendSrc, blendDst, blendAlphaEq, blendSrcAlpha, blendDstAlpha, blendColor);
-    };
-
-    setStencilEnabled (enabled) {
-        this._nativeObj.setStencilTest(enabled);
-    }
-
-    setStencil (enabled = gfx.STENCIL_INHERIT,
-        stencilFunc = gfx.DS_FUNC_ALWAYS,
-        stencilRef = 0,
-        stencilMask = 0xff,
-        stencilFailOp = gfx.STENCIL_OP_KEEP,
-        stencilZFailOp = gfx.STENCIL_OP_KEEP,
-        stencilZPassOp = gfx.STENCIL_OP_KEEP,
-        stencilWriteMask = 0xff) {
-        this._nativeObj.setStencil(stencilFunc, stencilRef, stencilMask, stencilFailOp, stencilZFailOp, stencilZPassOp, stencilWriteMask);
+    constructor(name, techniques, properties = {}, defines = {}, dependencies = [], buffer) {
+        super(name, techniques, properties, defines, dependencies, buffer);
+    
+        this._nativeObj = new renderer.EffectNative();
     }
 
     define(name, value) {
@@ -60,26 +21,25 @@ class NativeEffect extends Effect {
     getTechnique(stage) {
     }
 
-    setProperty (name, val) {
-        super.setProperty(name, val);
-
-        let prop = this._properties[name];
-        if (prop) {
-            this._nativeObj.setProperty(name, prop.value);
-        }
-    }
-
     clone () {
         let effect = super.clone();
-        effect._nativeObj = new renderer.EffectNative();
-        effect._nativeObj.copy(this._nativeObj);
-        effect._nativePtr = effect._nativeObj.self();
+        effect._nativeObj.copy(this._nativeObj, effect._buffer);
         return effect;
     }
 }
 
-Effect.parseTechniques = function () {
-    return [];
+let _parseEffect = cc.Effect.parseEffect;
+cc.Effect.parseEffect = function (asset) {
+    let effect = _parseEffect(asset);
+
+    let defines = effect._defines;
+    let definesArr = [];
+    for (let key in defines) {
+        definesArr.push({ name: key, value: defines[key] });
+    }
+    effect._nativeObj.init(effect._techniques, effect._properties, definesArr, effect._buffer);
+
+    return effect;
 }
 
 cc.Effect = NativeEffect;
